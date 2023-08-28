134fe75f25a9fa52381879cfde1329e2d0e3ae57
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-28 12:46:23 -0400

    Debugging issues...

--- core/parser.py
+++ core/parser.py
@@ -146,11 +146,7 @@ def parse(args):
             opt['train'].update(opt['debug'])
 
     ''' code backup '''
-    for name in os.listdir('.'):
-        if name in ['config', 'models', 'core', 'slurm', 'data', 'diffusion']:
-            shutil.copytree(name, os.path.join(opt['path']['code'], name), ignore=shutil.ignore_patterns("*.pyc", "__pycache__"))
-        if '.py' in name or '.sh' in name:
-            shutil.copy(name, opt['path']['code'])
+
     return dict_to_nonedict(opt)
 
 
